585766761997e3bd2469c03d9f766a2231b472d2
Andy Lawson <alawson89@gmail.com>
2021-01-22 10:33:01 -0800

    troubleshoot user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
-let array = [0,1,2,3];
-array.forEach(function(num) {
-  if (num === "1") {
-    alert("hi");
-  }
-})
+
 
 //User Interface Logic:
-$(document).ready(function() {
-  $('form').submit(function() {
+$(document).ready(function () {
+  $('form').submit(function (event) {
     event.preventDefault();
     let user = parseInt($('#num').val());
-    let return = beepBoop(user);
-    $('.confirmation').show;
+    $(".num").text(user);
+    $('.confirmation').show();
   });
 })
