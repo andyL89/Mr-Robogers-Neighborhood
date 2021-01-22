f09be340b23f38b365b072798a54d9966a11d643
Andy Lawson <alawson89@gmail.com>
2021-01-22 12:08:01 -0800

    meet 1st test specs/get closer to correct business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 function beepBoop(num) {
-  let response;
-  if (num === 1) {
+  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let response = [];
+  for (let index = 0; index <= num; index +=1) {
+    response.push(array[index]);
+  } if (num === 1) {
     response = 'Beep!';
   } if (num === 2) {
     response = 'Boop!';
