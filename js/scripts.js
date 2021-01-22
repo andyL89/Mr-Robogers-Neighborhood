c8b26ce25ebcc09307ccd6e7e8e53e7ff6c29925
Andy Lawson <alawson89@gmail.com>
2021-01-22 13:40:37 -0800

    switch back to edit branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 function beepBoop(num) {
   let array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  let beep = array.slice(1);
-  let boop = array.slice(2);
-  let wont = array.slice(3);
   let response = [];
   for (let index = 0; index <= num; index +=1) {
     response.push(array[index]);
