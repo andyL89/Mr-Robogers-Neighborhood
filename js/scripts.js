7d9152082299f817e4a1f9c2c9f46ffb88bb2954
Andy Lawson <alawson89@gmail.com>
2021-01-22 13:36:07 -0800

    experiment with business logic in 2nd branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 function beepBoop(num) {
-  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let beep = array.slice(1);
+  let boop = array.slice(2);
+  let wont = array.slice(3);
   let response = [];
   for (let index = 0; index <= num; index +=1) {
     response.push(array[index]);
-  } if (num === 1) {
-    response = 'Beep!';
-  } if (num === 2) {
-    response = 'Boop!';
-  } if (num === 3) {
+  } if (num == '1') {
+    response = "Beep!";
+  } if (num == '2') {
+    response = "Boop!";
+  } if (num == '3') {
     response = "Won't you be my neighbor?"
-  }  
+  } 
   return response;
 }
 
@@ -17,7 +20,7 @@ function beepBoop(num) {
 $(document).ready(function () {
   $('form').submit(function (event) {
     event.preventDefault();
-    let user = parseInt($('#num').val());
+    let user = $('#num').val();
     let response = beepBoop(user);
     $('.alert').text(response);
   });
