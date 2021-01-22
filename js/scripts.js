27736a94500555b624cd6bea28638bc458eff72f
Andy Lawson <alawson89@gmail.com>
2021-01-22 11:28:43 -0800

    create working basic albeit incomplete business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-
+function beepBoop(num) {
+  let response;
+  if (num === 1) {
+    response = 'Beep!';
+  } if (num === 2) {
+    response = 'Boop!';
+  } if (num === 3) {
+    response = "Won't you be my neighbor?"
+  }  
+  return response;
+}
 
 //User Interface Logic:
 $(document).ready(function () {
   $('form').submit(function (event) {
     event.preventDefault();
     let user = parseInt($('#num').val());
-    $(".num").text(user);
-    $('.confirmation').show();
+    let response = beepBoop(user);
+    $('.alert').text(response);
   });
 })
