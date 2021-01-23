b1a3a698e5bdf7083e4bf616ccfa0c25fe0fab7f
Andy Lawson <alawson89@gmail.com>
2021-01-22 16:45:19 -0800

    research .split method

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function beepBoop(num) {
-  let array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  array [1] = "Beep!";
-  array [2] = "Boop!";
-  array [3] = "Won't you be my neighbor?";
+  let array = ['0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9'];
+  array[1] = " Beep!";
+  array[2] = " Boop!";
+  array[3] = " Won't you be my neighbor?";
   let response = [];
-  for (let index = 0; index <= num; index +=1) {
+  for (let index = 0; index <= num; index++) {
     response.push(array[index]);
   } if (num == '1') {
     response = "Beep!";
@@ -12,9 +12,11 @@ function beepBoop(num) {
     response = "Boop!";
   } if (num == '3') {
     response = "Won't you be my neighbor?"
-  } 
+  }
   return response;
 }
+
+
 
 //User Interface Logic:
 $(document).ready(function () {
